64d70e314c66f6eaa2c9bda0ebc3f6e5a08b7f4c
BautiMM <bautista.mangold@gmail.com>
2021-06-06 20:47:58 -0300

    Tp4-Ej3
    Ejercicio 3.

---
+++ Tp4-ej3.py
@@ -0,0 +1,14 @@
+################
+# Juan Bautista Mangold Moro - @BautiMM
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Ejercicio Grados Celsius a Grados Fahrenheit
+
+fahrenheit = float(input("Ingrese grados Fahrenheit: "))
+centrigrados = float(fahrenheit - 32) * (5/9)
+print("Los grados Fahrenheit anteriormente puestos se traducen a: ", (centrigrados), "en grados Celsius")
+centigrados = float(input("Ingrese grados Celsius: "))
+fahrenheit = float(centigrados * 9/5) + 32
+print("Los grados Celsius anteriormente puestos se traducen a: ", (fahrenheit), "en grados Fahrenheit")
