15dc0681130185a743f6fab3ce5935a5809cba97
BautiMM <bautista.mangold@gmail.com>
2021-06-06 21:33:21 -0300

    Ejercicio 5

---
+++ Tp4-ej5.py
@@ -0,0 +1,15 @@
+################
+# Juan Bautista Mangold Moro - @BautiMM
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Ejercicio 5. Números positivos y negativos
+
+numero = int(input("Escriba un numero: " ))
+if numero < 0 : 
+        print("Es un numero negativo")
+if numero == 0 :
+        print("El numero es 0")
+if numero > 0 :
+        print("El numero es positivo")
