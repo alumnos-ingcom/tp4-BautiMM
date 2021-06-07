cb2edb8fe7f9baf8c5e2f90c6dd0095119be1c01
BautiMM <bautista.mangold@gmail.com>
2021-06-06 21:12:10 -0300

    Ejercicio 4

---
+++ Tp4-ej4.py
@@ -0,0 +1,17 @@
+################
+# Juan Bautista Mangold Moro - @BautiMM
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Ejercicio 4. Comparación de números
+
+numero = int(input("Escriba un numero: " ))
+numerodos = int(input("Escriba un segundo numero: " ))
+if numero < numerodos : 
+        print(-1)
+if numero == numerodos :
+        print(0)
+if numero > numerodos :
+        print(1)
+        
